7de86527aedb15551fdca51fefb6ff063b7bb985
bimadam <70703826+bimadam@users.noreply.github.com>
2021-05-13 23:26:58 +0200

    Add files via upload

---
+++ ceasar.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void caesar_encrypt (char message[])
+{
+    char abc;
+	int i;
+	int cislo;
+	
+	printf("Vas text: ");
+	gets(message);
+	printf("Kolik cisel chcete pouzit? ");
+	scanf("%d", &cislo);
+	
+	for (i = 0; message[i] != '\0'; ++i)
+	{
+		abc = message[i];
+		
+		if (abc >= 'A' && abc <= 'Z')
+		{
+			abc = abc + cislo;
+			
+			if (abc > 'Z'){
+				abc = abc - 'Z' + 'A' - 1;
+			}
+			
+			message [i] = abc;
+		}
+	}
+	
+	printf("Encrypted message: %s", message);
+	
+}
+int main()
+{
+	char abc;
+	int i;
+	int cislo;
+	char message [100];
+	
+	caesar_encrypt(message);
+	return 0;
+}
